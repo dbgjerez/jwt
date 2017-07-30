4b408ad715de377a4d3ecb08ef8d96cb5e3b910b
David Borrego <dbgjerez@gmail.com>
2017-07-30 21:10:50 +0200

    comprobación de usuario

--- src/main/java/com/dbg/jwt/service/UserService.java
+++ src/main/java/com/dbg/jwt/service/UserService.java
@@ -1,5 +1,10 @@
 package com.dbg.jwt.service;
 
+import com.dbg.jwt.dto.LoginDTO;
+import com.dbg.jwt.exceptions.InvalidUserException;
+
 public interface UserService {
+
+	Boolean checkUser(LoginDTO login) throws InvalidUserException;
 
 }
--- src/main/java/com/dbg/jwt/service/impl/UserServiceImpl.java
+++ src/main/java/com/dbg/jwt/service/impl/UserServiceImpl.java
@@ -1,10 +1,27 @@
 package com.dbg.jwt.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import com.dbg.jwt.dao.UserDao;
+import com.dbg.jwt.dto.LoginDTO;
+import com.dbg.jwt.exceptions.InvalidUserException;
+import com.dbg.jwt.model.user.User;
 import com.dbg.jwt.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Override
+	public Boolean checkUser(LoginDTO login) throws InvalidUserException {
+		final User user = new User();
+		user.setEmail(login.getEmail());
+		user.setPassword(login.getPassword());
+		return userDao.exists(Example.of(user));
+	}
 
 }
