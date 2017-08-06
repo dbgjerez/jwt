c889c67064e039daf9e577141eb00d4fe7d183de
David Borrego <dbgjerez@gmail.com>
2017-08-06 20:00:29 +0200

    query por nombre

--- src/main/java/com/dbg/jwt/dao/UserDao.java
+++ src/main/java/com/dbg/jwt/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.dbg.jwt.dao;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,7 @@ import com.dbg.jwt.model.user.User;
 
 @Repository
 public interface UserDao extends MongoRepository<User, ObjectId> {
+
+	public Optional<User> findOneByEmailAndPassword(String email, String password);
 
 }
