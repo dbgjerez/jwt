9841d369e6a435e2414dcddb406e6149fd12b471
David Borrego <dbgjerez@gmail.com>
2017-08-06 20:00:21 +0200

    lombok anotaciones

--- src/main/java/com/dbg/jwt/dto/LoginDTO.java
+++ src/main/java/com/dbg/jwt/dto/LoginDTO.java
@@ -2,27 +2,16 @@ package com.dbg.jwt.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginDTO implements Serializable {
 
 	private static final long serialVersionUID = -5138305849572270511L;
 
 	private String email;
 	private String password;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 }
