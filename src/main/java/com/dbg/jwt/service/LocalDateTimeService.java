436c06ca085a904ff9452b6584d0bf5847a0864c
David Borrego <dbgjerez@gmail.com>
2017-08-13 11:09:15 +0200

    método para parsear date

--- src/main/java/com/dbg/jwt/service/LocalDateTimeService.java
+++ src/main/java/com/dbg/jwt/service/LocalDateTimeService.java
@@ -1,11 +1,17 @@
 package com.dbg.jwt.service;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 public interface LocalDateTimeService {
 
 	LocalDateTime now();
 
 	LocalDateTime plusSeconds(LocalDateTime init, Integer seconds);
+
+	LocalDateTime toLocalDateTime(Date d);
+
+	LocalDateTime toLocalDateTime(Date d, ZoneId zoneId);
 
 }
