b20cebf7f37fd379f6a195cdc6600d4b3b21a55b
David Borrego <dbgjerez@gmail.com>
2017-08-19 18:09:44 +0200

    modificado el serivico para simplificación

--- src/main/java/com/dbg/jwt/service/JWTService.java
+++ src/main/java/com/dbg/jwt/service/JWTService.java
@@ -2,12 +2,13 @@ package com.dbg.jwt.service;
 
 import com.dbg.jwt.dto.GenerateTokenDTO;
 import com.dbg.jwt.dto.LoginDTO;
+import com.dbg.jwt.dto.ValidTokenDTO;
 import com.dbg.jwt.exceptions.InvalidUserException;
 
 public interface JWTService {
 
 	GenerateTokenDTO loginUser(LoginDTO login) throws InvalidUserException;
 
-	Boolean validateToken(String token);
+	ValidTokenDTO validateToken(String token);
 
 }
