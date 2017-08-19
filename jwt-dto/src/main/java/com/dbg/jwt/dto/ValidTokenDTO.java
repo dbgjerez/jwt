11c052b6177cf54ffe68e5b53df6552db0d79355
David Borrego <dbgjerez@gmail.com>
2017-08-19 18:09:32 +0200

    constructor para nombre de usuario

--- jwt-dto/src/main/java/com/dbg/jwt/dto/ValidTokenDTO.java
+++ jwt-dto/src/main/java/com/dbg/jwt/dto/ValidTokenDTO.java
@@ -2,9 +2,11 @@ package com.dbg.jwt.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ValidTokenDTO implements Serializable {
 
 	private static final long serialVersionUID = 3767909442204465399L;
